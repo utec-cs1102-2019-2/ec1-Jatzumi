d629538a83ab05b4b4c8947cb3c9b613825e9bfd
unknown <jatzumi.velazco@utec.edu.pe>
2019-09-11 17:26:03 -0500

    Subido

--- session4a/main.cpp
+++ session4a/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int ival=42;
+    int *p= &ival;
+    cout<<p<<"\t"<<*p<<"\t"<<&p;
     return 0;
 }
