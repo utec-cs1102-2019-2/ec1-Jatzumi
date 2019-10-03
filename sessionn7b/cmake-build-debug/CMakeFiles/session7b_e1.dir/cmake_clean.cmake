1283dbfa6a272795a00ceaf90832981e3d08e4b7
Jatzumi <54951222+Jatzumi@users.noreply.github.com>
2019-10-03 09:38:53 -0500

    session7b

---
+++ sessionn7b/cmake-build-debug/CMakeFiles/session7b_e1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session7b_e1.dir/ejercicio1.cpp.obj"
+  "session7b_e1.pdb"
+  "session7b_e1.exe"
+  "session7b_e1.exe.manifest"
+  "libsession7b_e1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session7b_e1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
